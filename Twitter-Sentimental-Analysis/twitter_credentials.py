6de51279eaf0d0ecd1c8aae5a70953bd610cf2ab
Ujjval Patel <30762887+Ujjval-Patel@users.noreply.github.com>
2018-12-20 14:36:15 +0530

    Update twitter_credentials.py

--- Twitter-Sentimental-Analysis/twitter_credentials.py
+++ Twitter-Sentimental-Analysis/twitter_credentials.py
@@ -1,7 +1,7 @@
 # variables that contain the user credentials to acess Twitter API
 
-ACCESS_TOKEN = "1074988906628534272-5o8fzvNmUN1C9bEoxY8ji4xff9AHif"
-ACCESS_TOKEN_SECRET = "jIO7FdVfMjwWBwY9ivmtkd2eK6HkTEzGpOEK3SB5szdHf"
-CONSUMER_KEY = "oI43oNAdz16H3vvuMD2Zxqneb"
-CONSUMER_SECRET = "wkDeS1M7IyHNmlTI5vgFRue0dvww4hgpKjOfMcwWuB9Q3ynm0Y"
+ACCESS_TOKEN = "Add your own"
+ACCESS_TOKEN_SECRET = "Add your own"
+CONSUMER_KEY = "Add your own"
+CONSUMER_SECRET = "Add your own"
 
